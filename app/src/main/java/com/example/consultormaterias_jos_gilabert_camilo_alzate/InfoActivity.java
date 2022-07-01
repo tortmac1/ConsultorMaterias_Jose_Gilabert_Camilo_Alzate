f894ca57d3248bb16f7786e6ba5fcd108259a1df
tortmac1 <tortmacdelaletra@gmail.com>
2022-07-01 23:27:34 +0200

    Aplicación de selector de materias, reparada y lista para revisión

--- app/src/main/java/com/example/consultormaterias_jos_gilabert_camilo_alzate/InfoActivity.java
+++ app/src/main/java/com/example/consultormaterias_jos_gilabert_camilo_alzate/InfoActivity.java
@@ -2,13 +2,36 @@ package com.example.consultormaterias_jos_gilabert_camilo_alzate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class InfoActivity extends AppCompatActivity {
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        ImageView imageInfo=null;
+        TextView textInfo=null;
+
+        imageInfo = findViewById(R.id.imagen_info);
+        textInfo = findViewById(R.id.texto_info);
+
+
+        int valor=0;
+
+         valor = getIntent().getExtras().getInt("valor");
+         //valor=1;
+
+         if(valor == 1){
+             imageInfo.setImageDrawable(getResources().getDrawable(R.drawable._24_original));
+             textInfo.setText(R.string.texto_vfx_completo);
+         }else{
+             imageInfo.setImageDrawable(getResources().getDrawable(R.drawable.animacion));
+             textInfo.setText(R.string.texto_animacion_completo);
+         }
     }
 }
--- app/src/main/java/com/example/consultormaterias_jos_gilabert_camilo_alzate/MainActivity.java
+++ app/src/main/java/com/example/consultormaterias_jos_gilabert_camilo_alzate/MainActivity.java
@@ -4,31 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1, button2;
+    Button btn_animacion, btn_vfx;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1 = (Button)findViewById(R.id.button1);
-        button2 = (Button)findViewById(R.id.button2);
+        btn_animacion = (Button)findViewById(R.id.btn_animacion);
+        btn_vfx = (Button)findViewById(R.id.btn_vfx);
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent button1 = new Intent(MainActivity.this,InfoActivity.class);
-                startActivity(button1);
-            }
-        });
+
     }
 
     @Override
     public void startActivity(Intent intent) {
         super.startActivity(intent);
+    }
+
+    public void selection(View view) {
+        Intent button1 = new Intent(MainActivity.this,InfoActivity.class);
+
+        switch(view.getId())
+        {
+            case R.id.btn_animacion:
+                //Operación animacion...
+                button1.putExtra("valor", 0);
+                break;
+            case R.id.btn_vfx:
+                //Operación vfx...
+                button1.putExtra("valor", 1);
+                break;
+
+        }
+        startActivity(button1);
+
+
+
     }
 
 }
